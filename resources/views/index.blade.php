d5f819c4478b0e438f1a8bd8a8897ef550f86e7c
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-11-05 11:38:28 -0500

    06 Laravel Paginator - Customize
    en el paginator de laravel ingresamos el codigo de nuestra plantilla para que la plantillade laravel adopte otro estilo de ver la paginacion.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,26 +34,8 @@
 
                 <div class="row pb120">
 
-                    <div class="col-lg-12">
-
-                        <nav class="navigation align-center">
-
-                            <a href="#" class="page-numbers bg-border-color current"><span>1</span></a>
-                            <a href="#" class="page-numbers bg-border-color"><span>2</span></a>
-                            <a href="#" class="page-numbers bg-border-color"><span>3</span></a>
-                            <a href="#" class="page-numbers bg-border-color"><span>4</span></a>
-                            <a href="#" class="page-numbers bg-border-color"><span>5</span></a>
-
-                            <svg class="btn-prev">
-                                <use xlink:href="#arrow-left"></use>
-                            </svg>
-                            <svg class="btn-next">
-                                <use xlink:href="#arrow-right"></use>
-                            </svg>
-
-                        </nav>
-
-                    </div>
+                    <div class="col-lg-12">{{ $products->links() }}</div>
+                    
 
                 </div>
             </div>
--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,36 +1,36 @@
 @if ($paginator->hasPages())
-    <ul class="pagination">
+    <nav class="navigation align-center">
         {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="disabled"><span>&laquo;</span></li>
-        @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-        @endif
+        {{--@if ($paginator->onFirstPage())--}}
+            {{--<li class="disabled"><span>&laquo;</span></li>--}}
+        {{--@else--}}
+            {{--<li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>--}}
+        {{--@endif--}}
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li class="disabled"><span>{{ $element }}</span></li>
-            @endif
+            {{--@if (is_string($element))--}}
+                {{--<li class="disabled"><span>{{ $element }}</span></li>--}}
+            {{--@endif--}}
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="active"><span>{{ $page }}</span></li>
+                        <a class="page-numbers bg-border-color current"><span>{{ $page }}</span></a>
                     @else
-                        <li><a href="{{ $url }}">{{ $page }}</a></li>
+                        <a href="{{ $url }}" class="page-numbers bg-border-color"><span>{{ $page }}</span></a>
                     @endif
                 @endforeach
             @endif
         @endforeach
 
         {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-        @else
-            <li class="disabled"><span>&raquo;</span></li>
-        @endif
-    </ul>
+        {{--@if ($paginator->hasMorePages())--}}
+            {{--<li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>--}}
+        {{--@else--}}
+            {{--<li class="disabled"><span>&raquo;</span></li>--}}
+        {{--@endif--}}
+    </nav>
 @endif
